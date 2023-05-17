3582e6494dad0cc8817bdba06bc00ca7ca626727
Shin-sangwon <dnjsml10@gmail.com>
2023-05-17 14:29:36 +0900

    refactor : cors 임시조치

--- src/main/java/com/ssafy/enjoytrip/security/configuration/SecurityConfig.java
+++ src/main/java/com/ssafy/enjoytrip/security/configuration/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.ssafy.enjoytrip.security.configuration;
 
 import com.ssafy.enjoytrip.member.model.service.MemberService;
+import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -37,6 +41,18 @@ public class SecurityConfig {
             .addFilterBefore(new JwtAuthenticationFilter(memberService, secretKey),
                 UsernamePasswordAuthenticationFilter.class)
             .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8081", "http://localhost:8080")); // 허용할 도메인 설정
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE")); // 허용할 HTTP 메서드 설정
+        configuration.setAllowedHeaders(Arrays.asList("*")); // 허용할 헤더 설정
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
