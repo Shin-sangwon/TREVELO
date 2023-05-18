f82d50b27e8970a8f5a01346ffeff076e0b90287
Shin-sangwon <dnjsml10@gmail.com>
2023-05-18 14:05:08 +0900

    feat : enum에 필드 추가

--- src/main/java/com/ssafy/enjoytrip/member/model/entity/Role.java
+++ src/main/java/com/ssafy/enjoytrip/member/model/entity/Role.java
@@ -1,5 +1,12 @@
 package com.ssafy.enjoytrip.member.model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum Role {
-    ADMIN, SELLER, MEMBER
+    ADMIN(9), SELLER(4), MEMBER(1);
+
+    private final Integer authLevel;
 }
