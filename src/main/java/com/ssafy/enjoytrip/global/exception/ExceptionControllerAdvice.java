9f2118d2865f9a88e33ee344ac202f1406a1ace9
Shin-sangwon <dnjsml10@gmail.com>
2023-05-19 17:54:51 +0900

    refactor : error log 추가

--- src/main/java/com/ssafy/enjoytrip/global/exception/ExceptionControllerAdvice.java
+++ src/main/java/com/ssafy/enjoytrip/global/exception/ExceptionControllerAdvice.java
@@ -5,16 +5,18 @@ import com.ssafy.enjoytrip.payment.exception.PaymentException;
 import com.ssafy.enjoytrip.reservation.exception.ReservationException;
 import com.ssafy.enjoytrip.room.exception.RoomException;
 import com.ssafy.enjoytrip.transaction.Exception.TransactionException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class ExceptionControllerAdvice {
 
     @ExceptionHandler(MemberException.class)
     public ResponseEntity<?> memberExceptionHandler(MemberException e) {
-
+        log.error("MemberException occurred: '{}'", e.getMessage(), e);
         return ResponseEntity.status(e.getErrorCode()
                                       .getHttpStatus())
                              .body(e.getErrorCode().name() + " " + e.getMessage());
@@ -22,6 +24,7 @@ public class ExceptionControllerAdvice {
 
     @ExceptionHandler(RoomException.class)
     public ResponseEntity<?> roomExceptionHandler(RoomException e) {
+        log.error("RoomException occurred: '{}'", e.getMessage(), e);
         return ResponseEntity.status(e.getErrorCode()
                                       .getHttpStatus())
                              .body(e.getErrorCode().name() + " " + e.getMessage());
@@ -29,6 +32,7 @@ public class ExceptionControllerAdvice {
 
     @ExceptionHandler(ReservationException.class)
     public ResponseEntity<?> reservationExceptionHandler(ReservationException e) {
+        log.error("ReservationException occurred: '{}'", e.getMessage(), e);
         return ResponseEntity.status(e.getErrorCode()
                                       .getHttpStatus())
                              .body(e.getErrorCode().name() + " " + e.getMessage());
@@ -36,6 +40,7 @@ public class ExceptionControllerAdvice {
 
     @ExceptionHandler(AmazonS3Exception.class)
     public ResponseEntity<?> amazonExceptionHandler(AmazonS3Exception e) {
+        log.error("AmazonS3Exception occurred: '{}'", e.getMessage(), e);
         return ResponseEntity.status(e.getErrorCode()
                                       .getHttpStatus())
                              .body(e.getErrorCode().name() + " " + e.getMessage());
@@ -43,13 +48,15 @@ public class ExceptionControllerAdvice {
 
     @ExceptionHandler(TransactionException.class)
     public ResponseEntity<?> transactionExceptionHandler(TransactionException e) {
+        log.error("TransactionException occurred: '{}'", e.getMessage(), e);
         return ResponseEntity.status(e.getErrorCode()
                                       .getHttpStatus())
                              .body(e.getErrorCode().name() + " " + e.getMessage());
     }
 
     @ExceptionHandler(PaymentException.class)
-    public ResponseEntity<?> transactionExceptionHandler(PaymentException e) {
+    public ResponseEntity<?> paymentExceptionHandler(PaymentException e) {
+        log.error("PaymentException occurred: '{}'", e.getMessage(), e);
         return ResponseEntity.status(e.getErrorCode()
                                       .getHttpStatus())
                              .body(e.getErrorCode().name() + " " + e.getMessage());
