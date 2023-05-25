028c50d64bc9572121b7c37541a98834dcc0c1a8
Shin-sangwon <dnjsml10@gmail.com>
2023-05-25 20:19:49 +0900

    feat : dto에 rating 추가

--- src/main/java/com/ssafy/enjoytrip/room/model/dto/response/RoomListResponseDto.java
+++ src/main/java/com/ssafy/enjoytrip/room/model/dto/response/RoomListResponseDto.java
@@ -1,6 +1,7 @@
 package com.ssafy.enjoytrip.room.model.dto.response;
 
 import com.ssafy.enjoytrip.room.model.entity.Room;
+import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,7 +16,7 @@ public class RoomListResponseDto {
     private String address;
     private Long pricePerNight;
     private String picture;
-
+    private BigDecimal rating;
     public static RoomListResponseDto from(Room entity) {
         return RoomListResponseDto.builder()
                                   .id(entity.getId())
